2d5b9ce380acbee7ccdd26dd081c46571bbebc91
trapvpack <vitost666@gmail.com>
2024-04-11 16:34:02 +0300

    completly configured project

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<h1 class="home">Заголовок</h1>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,5 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', [\App\Http\Controllers\IndexController::class, 'index'])->name('home');
